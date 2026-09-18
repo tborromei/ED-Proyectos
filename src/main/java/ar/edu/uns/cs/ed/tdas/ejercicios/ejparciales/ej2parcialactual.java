bda71b9a038c62e5114df1b9046033c17569c9fc
Tomi <tomasborromei13@gmail.com>
2026-09-18 11:21:50 -0300

    ....

--- src/main/java/ar/edu/uns/cs/ed/tdas/ejercicios/ejparciales/ej2parcialactual.java
+++ src/main/java/ar/edu/uns/cs/ed/tdas/ejercicios/ejparciales/ej2parcialactual.java
@@ -35,6 +35,74 @@ public class ej2parcialactual {
         return true;
     }
 
+    public static <E> boolean contenidaConAuxiliar(
+        PositionList<E> l1, PositionList<E> l2) {
+
+        PositionList<E> l3 = new ListaDobleEnlace<>();
+
+        if (l1.isEmpty()) {
+            return true;
+        }
+
+        Iterator<E> it1 = l1.iterator();
+        E buscado = it1.next();
+
+        // 1. Construimos L3 con las coincidencias en orden.
+        for (E actual : l2) {
+            if (buscado.equals(actual)) {
+                l3.addLast(actual);
+
+                if (!it1.hasNext()) {
+                    break; // Ya encontramos todo lo que pedia L1.
+                }
+
+                buscado = it1.next();
+            }
+        }
+
+        // 2. Comparamos L1 con L3.
+        if (l1.size() != l3.size()) {
+            return false;
+        }
+
+        Iterator<E> it3 = l3.iterator();
+
+        for (E elemento : l1) {
+            if (!elemento.equals(it3.next())) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static <E> boolean contenidaOrdenadamente(
+        PositionList<E> l1, PositionList<E> l2) {
+
+        Iterator<E> it1 = l1.iterator();
+        Iterator<E> it2 = l2.iterator();
+
+        while (it1.hasNext()) {
+            E buscado = it1.next();
+            boolean encontrado = false;
+
+            while (it2.hasNext() && !encontrado) {
+                E actual = it2.next();
+
+                if (buscado.equals(actual)) {
+                    encontrado = true;
+                }
+            }
+
+            if (!encontrado) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
     public static void main(String[] args) {
         PositionList<Character> l1 = new ListaDobleEnlace<>();
         PositionList<Character> l2 = new ListaDobleEnlace<>();
